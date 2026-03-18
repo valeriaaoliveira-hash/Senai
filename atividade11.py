748852f610394d3cb1ee85b42089595436320b34
valeriaaoliveira-hash <valeria_a_oliveira@estudante.sesisenai.org.br>
2026-03-18 18:06:15 +0000

    update

---
+++ atividade11.py
@@ -0,0 +1,15 @@
+print ("Bem- vindo ao sistema de gestão de estoque desenvolvido por valéria motta ")
+while True:
+     operacao = input ("deseja registrar a entrada e saida de produtos?(digite 'entrada' ou 'saida')ou 'sair'")
+     
+     if operacao =='sair':
+          break
+     
+ 
+
+
+
+elif input ("É entrada (e) ou saida (s) de um produto? ") == "n":
+     print ("Saida registrada no sistema")
+else:
+     print("")  
